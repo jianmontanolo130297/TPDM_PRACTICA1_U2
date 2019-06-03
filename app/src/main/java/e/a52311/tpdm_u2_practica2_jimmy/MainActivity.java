6940fa3dfe1791b1503f8b156896bdfb41b06f6d
jimmy montano <jianmontanolo@ittepic.edu.mx>
2019-06-03 10:40:00 -0600

    TPDM_UNIDAD2_JIMMY_P1

---
+++ app/src/main/java/e/a52311/tpdm_u2_practica2_jimmy/MainActivity.java
@@ -0,0 +1,13 @@
+package e.a52311.tpdm_u2_practica2_jimmy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
